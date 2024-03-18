b16f91d782e22a62320bd021d1782a93eee22bf3
Alex Clark <aclark@aclark.net>
2024-03-18 13:50:02 -0400

    Update project-makefile

--- project.mk
+++ project.mk
@@ -1,3 +1,3 @@
 install:
-	rez build --install
-	rez env hello_world_package -- python hello.py
+	/opt/rez/bin/rez/rez build --install
+	/opt/rez/bin/rez/rez env hello_world_package -- python hello.py
